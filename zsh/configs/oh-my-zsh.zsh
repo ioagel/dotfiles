cb05a5f76f7f473e211c888b85c03c75cfe2b946
Ioannis Angelakopoulos <ioangel@gmail.com>
2019-11-09 13:54:24 +0200

    Removes obsolete rubocop.yml, oh-my-zsh additions
    Changes search engine for ruby files

--- zsh/configs/oh-my-zsh.zsh
+++ zsh/configs/oh-my-zsh.zsh
@@ -1,10 +1,12 @@
 if [ -d "$HOME/.oh-my-zsh" ]; then
   export ZSH=$HOME/.oh-my-zsh
   ZSH_THEME="spaceship"
+  HYPHEN_INSENSITIVE="true"
+  HIST_STAMPS="dd/mm/yyyy"
+  export UPDATE_ZSH_DAYS=7
+  COMPLETION_WAITING_DOTS="true"
   plugins=(fasd brew colored-man-pages colorize common-aliases docker-compose
   docker dotenv github gitignore jsontools npm rails ruby safe-paste sudo themes
   tmux yarn zsh-completions zsh-syntax-highlighting history-substring-search)
-  HYPHEN_INSENSITIVE="true"
-  HIST_STAMPS="dd/mm/yyyy"
   source $ZSH/oh-my-zsh.sh
 fi
