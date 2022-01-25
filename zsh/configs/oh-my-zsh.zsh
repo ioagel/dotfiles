6c34d4d31d84209c284a3c2923ff0380ab7f7f68
Ioannis Angelakopoulos <ioangel@gmail.com>
2022-01-25 10:44:39 +0200

    fix zsh history bug

--- zsh/configs/oh-my-zsh.zsh
+++ zsh/configs/oh-my-zsh.zsh
@@ -2,7 +2,7 @@ if [ -d "$HOME/.oh-my-zsh" ]; then
   export ZSH=$HOME/.oh-my-zsh
   ZSH_THEME="spaceship"
   HYPHEN_INSENSITIVE="true"
-  HIST_STAMPS="dd/mm/yyyy"
+  HIST_STAMPS="dd.mm.yyyy"
   export UPDATE_ZSH_DAYS=7
   COMPLETION_WAITING_DOTS="true"
   plugins=(fasd brew colored-man-pages colorize common-aliases docker-compose
