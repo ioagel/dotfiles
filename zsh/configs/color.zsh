320c0e496ef1aede61fde8956d4431ad1340f4c8
Ioannis Angelakopoulos <ioagel@gmail.com>
2023-04-04 13:27:34 +0300

    fixes spotify i3 play/pause, setup alacritty colorscheme for day/night together with bat

--- zsh/configs/color.zsh
+++ zsh/configs/color.zsh
@@ -16,3 +16,7 @@ export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=3"
 # pygments default style
 export ZSH_COLORIZE_STYLE="solarized-light"
 
+LIGHT_COLOR='base16-one-light.yml'
+# LIGHT_COLOR='base16-solarized-light.yml'
+# LIGHT_COLOR='base16-tomorrow-night.yml'
+DARK_COLOR='base16-gruvbox-dark-hard.yml'
