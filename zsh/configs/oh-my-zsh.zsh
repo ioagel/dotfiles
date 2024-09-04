5c0fbfdad16d561b4a89f94ff608ad3b69ccbc00
Ioannis Angelakopoulos <ioagel@gmail.com>
2024-09-04 21:07:45 +0300

    re-arranges kubectl oh-my-zsh plugin to fix strange not registering bug

--- zsh/configs/oh-my-zsh.zsh
+++ zsh/configs/oh-my-zsh.zsh
@@ -6,10 +6,10 @@ if [ -d "$HOME/.oh-my-zsh" ]; then
   export UPDATE_ZSH_DAYS=7
   COMPLETION_WAITING_DOTS="true"
   plugins=(archlinux ssh-agent fasd brew colored-man-pages colorize
-          common-aliases docker-compose docker kubectl dotenv github gitignore
+          common-aliases docker-compose docker dotenv github gitignore
           jsontools npm safe-paste sudo themes tmux yarn vagrant
           history-substring-search asdf fzf zsh-syntax-highlighting
-          zsh-autosuggestions terraform z)
+          zsh-autosuggestions terraform z kubectl)
   ZSH_DISABLE_COMPFIX="true"
 
   source $ZSH/oh-my-zsh.sh
