410d59b5d53ce45e46abc07aeb8aff35dc247382
Ioannis Angelakopoulos <ioagel@gmail.com>
2025-05-02 17:43:14 +0300

    feat(picom): Optimize config for RX580/i3 and enhance visuals
    - Adjust blur, shadow, and damage settings for better performance.
    - Tweak shadow opacity and fading parameters for improved visual feel.
    - Change it's config file location (to ~/.config/picom/picom.conf)
    
    Change starship config file location (to ~/.config/starship/starship.conf)

--- zsh/configs/post/starship.zsh
+++ zsh/configs/post/starship.zsh
@@ -1,4 +1,5 @@
 # Initialize Starship
 # Load starship here, because when doing it as a oh-my-zsh plugin, it breaks the _load_settings function
 # The "post" directory contents are not loaded when doing it as a oh-my-zsh plugin
+export STARSHIP_CONFIG=~/.config/starship/starship.toml
 eval "$(starship init zsh)"
